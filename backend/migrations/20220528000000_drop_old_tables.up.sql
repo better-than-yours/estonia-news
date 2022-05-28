c928c6eee34cbe180eab31e86afd180702d4c83a
Sergei Kuvakin <sergey.kuvakin@coolbet.com>
2022-05-28 14:21:04 +0300

    feat: drop old tables

---
+++ backend/migrations/20220528000000_drop_old_tables.up.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS "old_entry_to_categories" CASCADE;
+DROP TABLE IF EXISTS "old_blocked_categories" CASCADE;
+DROP TABLE IF EXISTS "old_entries" CASCADE;
+DROP TABLE IF EXISTS "old_categories" CASCADE;
+DROP TABLE IF EXISTS "old_providers" CASCADE;
